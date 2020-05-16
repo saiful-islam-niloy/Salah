fb2c41a046ddfbca11358af7b7fde21eb20c5dcd
Saiful Islam Niloy <52848678+Saifulislam65@users.noreply.github.com>
2020-05-16 13:31:20 +0600

    Initial commit

---
+++ app/src/main/java/com/niloy/salah/SalahActivity.java
@@ -0,0 +1,13 @@
+package com.niloy.salah;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SalahActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_salah);
+    }
+}
