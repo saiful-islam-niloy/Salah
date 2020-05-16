311e321edd6b1c22db72f06189589add8f1a7012
Saiful Islam Niloy <52848678+Saifulislam65@users.noreply.github.com>
2020-05-16 16:44:59 +0600

    DataBase Added

---
+++ app/src/main/java/com/niloy/salah/DbHandler.java
@@ -0,0 +1,51 @@
+package com.niloy.salah;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.support.annotation.Nullable;
+import android.widget.Toast;
+
+public class DbHandler extends SQLiteOpenHelper {
+    private Context context;
+
+    private static final String DATABASE_NAME = "Salah.db";
+
+    private static final String TABLE_NAME = "Salah";
+    private static final String KEY_SALAH_ID = "_id";
+    private static final String KEY_SALAH_NAME = "name";
+    private static final String CREATE_SALAH_TABLE =
+            "CREATE TABLE "+TABLE_NAME+ "("+
+                    KEY_SALAH_ID+" INTEGER PRIMARY KEY AUTOINCREMENT, "+
+                    KEY_SALAH_NAME+" VARCHAR(255) );";
+
+    private static final int VERSION_NUMBER = 1;
+    private static final String DROP_TABLE = "DROP TABLE IF EXISTS "+ TABLE_NAME;
+
+    public DbHandler(@Nullable Context context) {
+        super(context, DATABASE_NAME, null, VERSION_NUMBER);
+        this.context = context;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        try{
+            db.execSQL(CREATE_SALAH_TABLE);
+            Toast.makeText(context, "Database Created. :)", Toast.LENGTH_LONG).show();
+        }catch (Exception e){
+            Toast.makeText(context, ""+e, Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        try{
+            db.execSQL(DROP_TABLE);
+            onCreate(db);
+            Toast.makeText(context, "Database Upgraded. :)", Toast.LENGTH_LONG).show();
+        }catch (Exception e){
+            Toast.makeText(context, ""+e, Toast.LENGTH_LONG).show();
+        }
+    }
+}
--- app/src/main/java/com/niloy/salah/SalahActivity.java
+++ app/src/main/java/com/niloy/salah/SalahActivity.java
@@ -1,13 +1,19 @@
 package com.niloy.salah;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class SalahActivity extends AppCompatActivity {
 
+    DbHandler dbHandler;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_salah);
+
+        dbHandler = new DbHandler(this);
+        SQLiteDatabase database =  dbHandler.getWritableDatabase();
+
     }
 }
